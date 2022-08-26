0fa91d7f2b5e8934cbc5ef5e94d0cb1a6cc73cfd
Kudinov Ivan <102667861+ikudin0v@users.noreply.github.com>
2022-08-26 13:40:13 +0300

    some functional and content

--- main.js
+++ main.js
@@ -1,17 +1,46 @@
 const App = {
     data() {
         return {
-            page: 'main',
+            page: 'catalog',
             loginState: 0,
-            showCatalog: 0,
+            showCatalog: false,
+            catalogPage: {
+                category: '',
+                subCategory: '',
+                quantity: 0,
+                goods: [],
+            },
         }
     },
     methods: {
-        showlog() {
-            console.log(1243234345)
+        showlog(a) {
+            console.log(a)
+        },
+        changePage(newPage) {
+            console.log(newPage)
+            this.page = newPage
+            console.log(this.page)
+        },
+        showCategory (category, manufacturer, subCategory) {
+            this.catalogPage.category = ''
+            this.catalogPage.subCategory = ''
+            this.catalogPage.quantity = 0
+            this.catalogPage.goods = []
+            fetch('./Data/Catalog.json')
+                .then((response) => response.json())
+                .then((data) => {
+                    this.catalogPage.category = category
+                    this.catalogPage.subCategory = subCategory
+                    data.catalog.forEach(element => {
+                        if ((element.name != '') && (element.category == category) && (element.manufacturer == manufacturer)) {
+                            this.catalogPage.goods.push(element.name)
+                        }
+                    });
+                    console.log(this.catalogPage.goods)
+                })
         }
     }
 }
 
-Vue.createApp(App).mount('#main')
-Vue.createApp(App).mount('#navBar')
+Vue.createApp(App).mount('body')
+// Vue.createApp(App).mount('#navBar')
