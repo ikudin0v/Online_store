1cc13aa5e2bc47e97722f726c13733075ce63967
Kudinov Ivan <102667861+ikudin0v@users.noreply.github.com>
2022-09-04 23:16:38 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 const App = {
     data() {
         return {
-            page: 'catalog',
+            catalog: [],
+            page: 'home',
             loginState: 0,
             showCatalog: false,
+            logInUp: '',
             catalogPage: {
                 category: '',
                 subCategory: '',
-                quantity: 0,
-                goods: [],
+                manufacturer: '',
+                goodsID: [],
             },
+            itemPage: {
+                id: '',
+                name: '',
+                price: '',
+                img:'',
+                description: []
+            }
         }
     },
     methods: {
@@ -22,25 +31,74 @@ const App = {
             console.log(this.page)
         },
         showCategory (category, manufacturer, subCategory) {
-            this.catalogPage.category = ''
-            this.catalogPage.subCategory = ''
-            this.catalogPage.quantity = 0
-            this.catalogPage.goods = []
-            fetch('./Data/Catalog.json')
-                .then((response) => response.json())
-                .then((data) => {
-                    this.catalogPage.category = category
-                    this.catalogPage.subCategory = subCategory
-                    data.catalog.forEach(element => {
-                        if ((element.name != '') && (element.category == category) && (element.manufacturer == manufacturer)) {
-                            this.catalogPage.goods.push(element.name)
-                        }
-                    });
-                    console.log(this.catalogPage.goods)
-                })
+            this.catalogPage.category = category
+            this.catalogPage.subCategory = subCategory
+            this.catalogPage.manufacturer = manufacturer
+            this.catalogPage.goodsID = []
+
+            App.catalog.forEach(element => {
+                if ((element.category == category) && (element.manufacturer == manufacturer) && (subCategory == undefined)) {
+                    this.catalogPage.goodsID.push(element)
+                } else if ((element.category == category) && (manufacturer == undefined) && (element.subCategory == subCategory)) {
+                    this.catalogPage.goodsID.push(element)
+                }
+            });
+            this.page = 'catalog'
+        },
+        showItem(id) {
+            console.log(id)
+            App.catalog.forEach(element => {
+                if (element.id == id) {
+                    this.itemPage.id = element.id
+                    this.itemPage.name = element.name
+                    this.itemPage.price = element.price
+                    this.itemPage.img = element.img
+                    this.itemPage.description = element.description
+                }
+            })
+            this.page = 'item'
+        },
+        showLoginWindow() {
+            this.logInUp = 'login'
         }
     }
 }
 
 Vue.createApp(App).mount('body')
-// Vue.createApp(App).mount('#navBar')
+fetch('./Data/Catalog.json')
+    .then((response) => response.json())
+    .then((data) => {
+        App.catalog = data.catalog
+    })
+
+
+
+
+
+
+// showCategory (category, manufacturer, subCategory) {
+//     this.catalogPage.category = category
+//     this.catalogPage.subCategory = subCategory
+//     this.catalogPage.manufacturer = manufacturer
+//     this.catalogPage.goodsID = []
+    
+    
+    
+//     if (this.catalog == {}) {}
+//     fetch('./Data/Catalog.json')
+//         .then((response) => response.json())
+//         .then((data) => {
+            
+//             console.log (this.catalogPage.category,  this.catalogPage.subCategory,  this.catalogPage.manufacturer)
+//             data.catalog.forEach(element => {
+//                 if ((element.category == category) && (element.manufacturer == manufacturer) && (subCategory == undefined)) {
+//                     this.catalogPage.goodsID.push(element)
+//                     console.log(element)
+//                 } else if ((element.category == category) && (manufacturer == undefined) && (element.subCategory == subCategory)) {
+//                     this.catalogPage.goodsID.push(element)
+//                     console.log(element)
+//                 }
+//             });
+//             this.page = 'catalog'
+//         })
+// },
